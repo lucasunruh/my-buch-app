1d2af9a0ab7b6ce43ee979baa9fa26940037bf6b
Lucas Unruh <lucasdunruh@gmail.com>
2021-03-16 18:51:48 -0400

    Removing db connection string
    Removing for security purposes

--- database.js
+++ database.js
@@ -1,6 +1,5 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(process.env.JAWSDB_URL);
-//var connection = mysql.createConnection('mysql://urjvicx8lx49rwh9:jkb8nqs78171ph3c@eyw6324oty5fsovx.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/tyb9mf2bg36eaz5o');
 
 connection.connect(function(err) {
   if (err) {
